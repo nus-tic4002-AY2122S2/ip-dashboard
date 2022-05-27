d4b7dcea19b69f17c5f8d8223f4b37cd20d45e17
Damith C. Rajapakse <damith@gmail.com>
2022-05-27 10:27:40 +0800

    Set end date

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 
 ./get-reposense.py --master
-java -jar RepoSense.jar --config ./configs --since 10/01/2022  --formats java md fxml sh bat gradle txt --timezone UTC+08 --last-modified-date
+java -jar RepoSense.jar --config ./configs --since 10/01/2022 --until 30/04/2022  --formats java md fxml sh bat gradle txt --timezone UTC+08 --last-modified-date
